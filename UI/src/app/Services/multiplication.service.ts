d4eccf3a9b6f9f1d36197b6a916e26b7402d8c01
Yassin <Yassin@DESKTOP-0HHI7H9.lan>
2018-07-29 00:54:47 +0200

    Adding post handling method

--- UI/src/app/Services/multiplication.service.ts
+++ UI/src/app/Services/multiplication.service.ts
@@ -1,4 +1,5 @@
 import {Multiplication} from '../Model/multiplication';
+import {MultiplicationResultAttempt} from '../Model/multiplicationResultAttempt';
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
@@ -6,12 +7,23 @@ import {Observable} from 'rxjs';
 @Injectable()
 export class MultiplicationService {
 
-  private URL = 'http://localhost:8080/multiplications/random';
+  private GET_URL = 'http://localhost:8080/multiplications/random';
+  private POST_URL = 'http://localhost:8080/results';
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
 
   constructor(private http: HttpClient) {}
 
   public getMultiplication(): Observable<Multiplication> {
-    return this.http.get<Multiplication>(this.URL);
+    return this.http.get<Multiplication>(this.GET_URL);
+  }
+
+  public postMultiplication(resultAttempt: MultiplicationResultAttempt) {
+    this.http.post<MultiplicationResultAttempt>(this.POST_URL, resultAttempt, this.httpOptions);
   }
 
 }
